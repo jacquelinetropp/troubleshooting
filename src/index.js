51a83d6120fc09f5ba1f5663c9f6d5a4e5893a33
Jacqueline Tropp <jacquelinetropp@gmail.com>
2021-02-15 20:31:02 -0600

    troubleshooting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from "react-router-dom";
+import { Provider, useSelector } from "react-redux";
+import store from "./store";
+import firebase from './firebase/firebase';
+
+import { ReactReduxFirebaseProvider, getFirebase, isLoaded } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore'
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
+  attachAuthIsReady: true,
+};
+
+// react-redux-firebase config
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
+
+const root = document.getElementById("root");
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+    <Fragment>
+     Hi
+    </Fragment>
+,
+  root
 );
+
+function AuthIsLoaded({children}) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <div>Loading screen...</div>
+  return children
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root')
+)
+
+// store.firebaseAuthIsReady.then(() => {
+//   ReactDOM.render(
+//     <Provider store={store}>
+//     <ReactReduxFirebaseProvider {...rrfProps}>
+//       <BrowserRouter>
+ 
+//           <Fragment>
+
+//             <App />
+//           </Fragment>
+   
+//       </BrowserRouter>
+//       </ReactReduxFirebaseProvider>
+//     </Provider>,
+//     root
+//   );
+// });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import firebase from "../firebase/firebase";
+import { getFirebase } from "react-redux-firebase";
+import {  getFirestore } from "redux-firestore";
+
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
+
+export default store;
